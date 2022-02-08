a245611d4c7d05ca2d7c0c687d4f8204a3e1bd23
Layla <layla@Laylas-MacBook-Pro.local>
2022-02-08 11:54:36 -0600

    added documentation

--- R/phers.R
+++ R/phers.R
@@ -1,37 +1,87 @@
 #' @import checkmate
-#' @importFrom data.table data.table := set
+#' @importFrom data.table data.table := set uniqueN .N
+#' @importFrom foreach foreach %do% %dopar%
+#' @importFrom stats lm rstudent
 NULL
 
 
+#' Calculate phenotype weights
 #'
-#' @param GRIDs vector of person IDs for everyone in the population
-#' @param phecodes [GRID, phecode]
-calcWeights = function(GRIDs, phecodes) {
+#' Calculate the population prevalence and weight for each phenotype.
+#' These weights will be used in the calculation of the phenotype risk score.
+#'
+#' @param demos A data.table containing demographic information for each person
+#'   in the population. The columns are `person_ID`, `sex`, `uniq_age`,
+#'   `first_age`, and `last_age`. The age columns specify the number of unique years with
+#'   ICD codes, the age of the individual at first ICD code,
+#'   and the age of the individual at last ICD code.
+#' @param phecodes A data.table containing phenotypes stored as phecodes
+#'   for each person. The columns are `person_ID` and `phecode`.
+#'
+#' @return A data.table where each row is a phecode and the population
+#'   prevalence and weight corresponding to it.
+#'   The columns are `phecode`, `prev`, `w`.
+#'
+#' @export
+calcWeights = function(demos, phecodes) {
+  phecode = `.` = NULL
 
-  npop = uniqueN(GRIDs)
+  npop = uniqueN(demos$person_ID)
   weights = phecodes[, .(prev = .N / npop, w = -log(.N / npop)), by = phecode]
 
 return(weights)}
 
+
+#' Calculate the Phenotype Risk Score
 #'
-#' @param demos vector of person IDs for everyone in the population
-#' @param phecodes [GRID, phecode]
-#' @param weights  [phecode, w]
-#' @param diseasePhecodeMap [dID, phecode]
+#' Calculate the phenotype risk score (PheRS)
+#' for a group of individuals and diseases
+#'
+#'
+#' @param demos A data.table containing demographic information for each person
+#'   in the population. The columns are `person_ID`, `sex`, `uniq_age`,
+#'   `first_age`, and `last_age`. The age columns specify the number of unique years with
+#'   ICD codes, the age of the individual at first ICD code,
+#'   and the age of the individual at last ICD code.
+#' @param phecodes A data.table containing phenotypes stored as phecodes
+#'   for each person. The columns are `person_ID` and `phecode`.
+#' @param weights A data.table where each row is a phecode and the weight
+#'   corresponding to it. The columns are `phecode` and `w`.
+#' @param diseasePhecodeMap A data.table containing the mapping between
+#'   diseases and their phenotypes. The columns are `disease_ID`, `phecode`.
+#' @param diseaseIDs vector of disease IDs to calculate PheRS for.
+#'
+#' @return A data.table of raw and residualized phenotype risk scores
+#'   with one row per person per disease. The columns are `person_ID`,
+#'   `disease_ID`, `phers`, `rphers`.
+#'
+#' @export
 calcPheRS = function(demos, phecodes, weights, diseasePhecodeMap, diseaseIDs){
+
+  person_ID = disease_ID = ID = w = rphers = `.` = NULL
 
   phecodesW = merge(phecodes, weights, by='phecode')
 
   phersAll = foreach (ID = unique(diseaseIDs),
                         .combine = rbind) %dopar% {
 
-
-              phecodesWSub = merge(phecodesW, diseasePhecodeMap[diseaseID==ID],
+              phecodesWSub = merge(phecodesW, diseasePhecodeMap[disease_ID==ID],
                                 by = 'phecode', allow.cartesian = TRUE)
-              phers = phecodesWSub[, .(phers = sum(w)), by = .(GRID, dID)]
+              phers = phecodesWSub[, .(phers = sum(w)), by = 'person_ID']
 
-              phers = merge(demo, phers, by='GRID', all.x = TRUE)
+              phers = merge(demos, phers, by='person_ID', all.x = TRUE)
               phers[is.na(phers),phers:=0]
-              phers[, diseaseID:=ID]}
+              phers[, disease_ID:=ID]
+
+
+              rphersFit = lm(phers~sex+uniq_age+first_age+last_age,
+                            data=phers)
+              phers[,rphers:=rstudent(rphersFit)]
+
+              phers[,.(person_ID, disease_ID, phers, rphers)]}
 
 return(phersAll)}
+
+
+
+
